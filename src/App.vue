5b03467f977fddd8b25390993daec1c344867983
Edson Lopes <dev_edson@outlook.com>
2023-03-30 10:57:38 -0300

    page structure commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar :logo="logo_src" :alt="alt_name" />
     <router-view />
     <Footer />
   </div>
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Navbar,
-    Footer
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burguer",
+    };
   },
 };
 </script>
 
 <style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
